d681f06e72e0288dc971f3fafde401f61332b527
Vchen7629 <chenvincent7629@gmail.com>
2026-03-13 22:01:56 -0700

    wip(backend client): image processing function

--- backend/client/internal/service/image.go
+++ backend/client/internal/service/image.go
@@ -1,25 +1,41 @@
 package service
 
 import (
+	"context"
 	"encoding/base64"
 	"fmt"
 
 	"github.com/gorilla/websocket"
+	fd "mosaic-client.com/gen/face_detection"
 )
 
-func ProcessImageFrame(frameData string, patientID string, conn *websocket.Conn) error {
+func ProcessImageFrame(
+	frameData string, 
+	patientID string, 
+	conn *websocket.Conn,
+	client fd.FaceDetectionServiceClient,
+) error {
+	ctx := context.Background()
 	// Decode base64
-	_, err := base64.StdEncoding.DecodeString(frameData)
+	faceBytes, err := base64.StdEncoding.DecodeString(frameData)
 	if err != nil {
 		return fmt.Errorf("Frame decode error: %w", err)
 	}
 
+	//log.Printf("[ProcessFace] Recieved face_data: %s", frameData)
+
 	// Compress JPEG frame for better performance
 
 	// Send to face detection service via gRPC
-	result := "hi"
+	resp, err := client.ProcessFaces(ctx, &fd.ProcessFacesRequest{
+		FaceBytes: faceBytes,
+		PatientId: patientID,
+	})
+	if err != nil {
+
+	}
 
 	// Send result back to frontend?
-	conn.WriteJSON(result)
+	conn.WriteJSON(resp)
 	return nil
 }
