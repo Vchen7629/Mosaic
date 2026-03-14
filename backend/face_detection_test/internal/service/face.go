8fc64a20c6d1614d6b4b6396112dad566612d832
Vchen7629 <chenvincent7629@gmail.com>
2026-03-14 16:56:51 -0700

    feat(face_detection): created face related logic function

--- backend/face_detection_test/internal/service/face.go
+++ backend/face_detection_test/internal/service/face.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"log"
 	"path/filepath"
 
@@ -10,15 +11,64 @@ import (
 var modelsDir = filepath.Join("models")
 
 // initializes the face detection model
-func InitializeFaceDetector() (*face.recognizer, error){
+func InitializeFaceDetector() (*face.Recognizer, error) {
 	rec, err := face.NewRecognizer(modelsDir)
 	if err != nil {
-		return nil, log.Fatalf("Can't init face recognizer: %v", err)
+		log.Fatalf("can't init face recognizer: %v", err)
 	}
 
 	return rec, nil
 }
 
-func GenerateFaceEmbeddings(faceBytes []byte) {
+// takes raw image bytes, runs face detection, and returns
+// the 128-d  descriptor for the first detected face
+func GenerateFaceEmbeddings(
+	rec *face.Recognizer, 
+	faceBytes []byte,
+) ([]face.Descriptor, error) {
+	faces, err := rec.Recognize(faceBytes)
+	if err != nil {
+		return nil, fmt.Errorf("recognition failed: %w", err)
+	}
 	
+	embeddings := make([]face.Descriptor, len(faces))
+	for i, f := range faces {
+		embeddings[i] = f.Descriptor
+	}
+
+	return embeddings, nil
+}
+
+// visitor from database
+type KnownVisitor struct {
+	ID			int32
+	Embedding 	face.Descriptor
+}
+
+// Compare face embeddings of visitors
+// saved in the database to the new face
+// embeddings sent by the frontend
+func CompareFaces(
+	rec *face.Recognizer,
+	embeddings []face.Descriptor,
+	knownVisitors []KnownVisitor,
+) map[int]int32 { // index of detected_face -> visitor_id (-1 if unknown)
+	knownEmbeddings := make([]face.Descriptor, len(knownVisitors))
+	knownIDs := make([]int32, len(knownVisitors))
+
+ 	for i, v := range knownVisitors {
+		knownEmbeddings[i] = v.Embedding
+		knownIDs[i] = v.ID
+	}
+	rec.SetSamples(knownEmbeddings, knownIDs)
+
+	results := make(map[int]int32, len(embeddings))
+	for i, embedding := range embeddings {
+		results[i] = int32(rec.ClassifyThreshold(embedding, 0.6))
+	}
+
+	// returns dict with key: index of detected face in frame (0, 1, 2,..)
+	// and value: visitor_id from db if matched, or -1 if unknown
+	// example: face 0 and 2 are known: {0: 5, 1: -1, 2: 3}
+	return results
 }
