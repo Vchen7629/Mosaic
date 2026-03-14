476b1dbc9113086a8b1fb34b0037230041f24744
Vchen7629 <chenvincent7629@gmail.com>
2026-03-14 16:53:05 -0700

    feat(face_detection): created connection.go to handle postgres db connection

---
+++ backend/face_detection_test/internal/db/connection.go
@@ -0,0 +1,46 @@
+package db
+
+import (
+	"context"
+	"log"
+	"time"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+
+type DBPool struct {
+	pool *pgxpool.Pool
+}
+
+// allows us to initialize the db pool in main.go
+func NewDBPool(pool *pgxpool.Pool) *DBPool {
+	return &DBPool{pool: pool}
+}
+
+// set up pgxpool db connection pool
+func ConnectionPool(database_url string) *pgxpool.Pool {
+	pool, err := pgxpool.ConnectConfig(
+		context.Background(), databaseConfig(database_url),
+	)
+	if err != nil {
+		log.Fatalf("Unable to connect to database %v\n", err)
+	}
+
+	return pool
+}
+
+// Pgxpool postgres connection pool config values
+func databaseConfig(database_url string) *pgxpool.Config { 
+	config, err := pgxpool.ParseConfig(database_url)
+	if err != nil {
+		log.Fatalf("Unable to parse DATABASE_URL %v\n", err)
+	}
+
+	config.MaxConns = 50
+	config.MinConns = 5
+	config.MaxConnLifetime = 30 * time.Minute
+	config.MaxConnIdleTime = 5 * time.Minute
+	config.HealthCheckPeriod = time.Minute
+
+	return config
+}
