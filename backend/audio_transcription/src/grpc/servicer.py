eea29fedb06d93172fa89e2421914f5f629c429f
Vchen7629 <chenvincent7629@gmail.com>
2026-03-16 17:30:01 -0700

    feat(audio_transcription): finished the SaveTranscript gRPC method to read from log txt file and save to db

--- backend/audio_transcription/src/grpc/servicer.py
+++ backend/audio_transcription/src/grpc/servicer.py
@@ -1,9 +1,10 @@
 from psycopg_pool import ConnectionPool
-from ..transcription.handler import transcribe_handler
 from ..core.settings import _LOGS_DIR
 from .. import audio_transcription_pb2
 from .. import audio_transcription_pb2_grpc
-from ..transcription.log import LogWriter
+from ..db.queries import save_conversation
+from ..transcription.log import ConvoLogHandler
+from ..transcription.handler import transcribe_handler
 import threading
 import numpy as np
 
@@ -11,32 +12,42 @@ class AudioTranscriptionServicer(
     audio_transcription_pb2_grpc.AudioTranscriptionServiceServicer
 ):
     def __init__(self, db_pool: ConnectionPool) -> None:
-        self._log_writers: dict[str, LogWriter] = {}
+        self._convo_loggers: dict[str, ConvoLogHandler] = {}
         self._lock = threading.Lock()
-        self._db_pool = db_pool
+        self._db_pool: ConnectionPool = db_pool
 
     def TranscribeAudio(self, request, _context):
         chunk = np.array(request.audio_bytes, dtype=np.float32)
         print(f"[audio_transcription] got chunk {chunk}")
         text = transcribe_handler(chunk)
         if text:
-            self._get_log_writer(request.patient_id).write(text)
+            self._get_log_writer(request.profile_id).write(text)
 
         return audio_transcription_pb2.TranscribeAudioResponse(success=text is not None)
 
     def SaveTranscript(self, request, _context):
         """gRPC handler that saves the current transcript to db"""
+        convo_text = self._convo_loggers[request.patient_id].read()
+
+        try:
+            with self._db_pool.connection() as conn:
+                save_conversation(conn, request.profile_id, convo_text, request.visitor_id)
+        except Exception:
+            return audio_transcription_pb2.SaveTranscriptResponse(success=False)
+
+        self._convo_loggers[request.patient_id].delete()
+
         print("Invoked Save!")
         return audio_transcription_pb2.SaveTranscriptResponse(success=True)
 
-    def _get_log_writer(self, patient_id: str) -> LogWriter:
+    def _get_log_writer(self, patient_id: str) -> ConvoLogHandler:
         """
         Creates the log file with patient_id specific name
         and registers the log writer for the patient to dict
         """
         with self._lock:
-            if patient_id not in self._log_writers.keys():
+            if patient_id not in self._convo_loggers.keys():
                 _LOGS_DIR.mkdir(parents=True, exist_ok=True)
                 path = _LOGS_DIR / f"recording_{patient_id}.txt"
-                self._log_writers[patient_id] = LogWriter(path)
-            return self._log_writers[patient_id]
+                self._convo_loggers[patient_id] = ConvoLogHandler(path)
+            return self._convo_loggers[patient_id]
