870dc27e27d05b406bab16d4c9a77b64e39a105e
Vchen7629 <chenvincent7629@gmail.com>
2026-03-19 14:39:26 -0700

    tests(convo_briefing): added unit tests for build_prompt

---
+++ backend/conversation_briefing/internal/service/llm_prompt_test.go
@@ -0,0 +1,34 @@
+//go:build unit
+
+package service_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"mosaic-conversation-briefing.com/internal/service"
+)
+
+func TestBuildPrompt(t *testing.T) {
+
+	t.Run("missing convo text returns an error", func(t *testing.T) {
+		prompt, err := service.BuildPrompt([]string{})
+
+		assert.Nil(t, prompt)
+		assert.Equal(t, "missing conversation strings", err.Error())
+	})
+
+	t.Run("it builds prompt with the conversation text", func(t *testing.T) {
+		prompt, err := service.BuildPrompt([]string{"test prompt"})
+
+		assert.Equal(t, "Please summarize the follow visitor conversations into a concise briefing:\n\nConversation 1:\ntest prompt\n\nProvide a 3-4 sentence summary of the key topics, and conversation starters", *prompt)
+		assert.Nil(t, err)
+	})
+
+	t.Run("it builds prompt with multiple conversations", func(t *testing.T) {                                                                                                     
+		prompt, err := service.BuildPrompt([]string{"convo 1", "convo 2"})                                                                                                         
+									
+		assert.Nil(t, err)
+		assert.Equal(t, "Please summarize the follow visitor conversations into a concise briefing:\n\nConversation 1:\nconvo 1\n\nConversation 2:\nconvo 2\n\nProvide a 3-4 sentence summary of the key topics, and conversation starters", *prompt)
+  })
+}
