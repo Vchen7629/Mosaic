5c0564b0d7a719291a2f9c9d0b98689fcf652f49
Vchen7629 <chenvincent7629@gmail.com>
2026-03-19 15:14:56 -0700

    feat(backend_client): added gRPC call to briefing service after save transcript

--- backend/client/internal/handler/websocket.go
+++ backend/client/internal/handler/websocket.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gorilla/websocket"
 	at "mosaic-client.com/gen/audio_transcription"
 	fd "mosaic-client.com/gen/face_detection"
+	cb "mosaic-client.com/gen/conversation_briefing"
 	"mosaic-client.com/internal/service"
 )
 
@@ -25,8 +26,9 @@ type Message struct {
 }
 
 type WebSocketHandler struct {
-	AudioClient at.AudioTranscriptionServiceClient
-	FaceClient  fd.FaceDetectionServiceClient
+	AudioClient 	at.AudioTranscriptionServiceClient
+	FaceClient  	fd.FaceDetectionServiceClient
+	BriefingClient 	cb.ConversationBriefingServiceClient
 }
 
 var upgrader = websocket.Upgrader{
@@ -37,6 +39,7 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+// Handles routing websocket messages to the correct gRPC goroutine routes to do processing
 func (h *WebSocketHandler) HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 	log.Printf("[WebSocket] Connection attempt from origin: %s", r.Header.Get("Origin"))
 
@@ -65,43 +68,70 @@ func (h *WebSocketHandler) HandleWebSocket(w http.ResponseWriter, r *http.Reques
 
 		switch msg.Type {
 		case "sync_profile":
-			go func(m Message) {
-				err := service.SyncProfile(m.Frames, safe, h.FaceClient)
-				if err != nil {
-					log.Printf("[sync_profile] error: %v", err)
-				}
-			}(msg)
+			go h.handleSyncProfile(msg, safe)
 		case "visitor_face":
-			go func(m Message) {
-				err = service.ProcessVisitorImage(m.FaceBytes, m.ProfileID, safe, h.FaceClient)
-				if err != nil {
-					log.Printf("[visitor_face] error: %v", err)
-				}
-			}(msg)
+			go h.handleProcessVisitorFace(msg, safe)
 		case "new_visitor_face":
-			go func(m Message) {
-				err = service.RegisterNewVisitorFace(m.FaceEmbedding, m.ProfileID, m.VisitorName, safe, h.FaceClient)
-				if err != nil {
-					log.Printf("[new_visitor_face] error: %v", err)
-				}
-			}(msg)
+			go h.handleRegisterVisitorFace(msg, safe)
 		case "audio":
 			service.Wg.Add(1)
-			go func(m Message) {
-				defer service.Wg.Done()
-				err = service.ProcessAudio(m.AudioData, m.ProfileID, h.AudioClient)
-				if err != nil {
-					log.Printf("[audio] error: %v", err)
-				}
-			}(msg)
+			go h.handleAudio(msg, safe)
 		case "save_audio_transcript":
-			ctx := context.Background()
-			service.FlushAudio(ctx, msg.ProfileID, h.AudioClient)
-			err = service.SaveTranscriptWithRetry(ctx, msg.ProfileID, msg.VisitorIDList, h.AudioClient)
-			if err != nil {
-				log.Printf("error saving transcript: %v", err)
-			}
-			service.ClearSeenVisitors() // clear all the seen ids for the session
+			h.handleSaveAudioTranscript(msg)
 		}
 	}
+}
+
+// sends the message to sync profile to do face detection and sync user profile
+func (h *WebSocketHandler) handleSyncProfile(m Message, safe *service.SafeConn) {
+	err := service.SyncProfile(m.Frames, safe, h.FaceClient)
+	if err != nil {
+		log.Printf("[sync_profile] error: %v", err)
+	}
+}
+
+// runs every 2.5 frames, checks the sent face frame against visitors and decides
+// whether to register a new face or fetch briefing for an existing face
+func (h *WebSocketHandler) handleProcessVisitorFace(m Message, safe *service.SafeConn) {
+	err := service.ProcessVisitorImage(m.FaceBytes, m.ProfileID, safe, h.FaceClient)
+	if err != nil {
+		log.Printf("[visitor_face] error: %v", err)
+	}
+}
+
+// if the visitor face doesnt match a face in the db, this allows us to register the face
+// as a new visitor face with the name
+func (h *WebSocketHandler) handleRegisterVisitorFace(m Message, safe *service.SafeConn) {
+	err := service.RegisterNewVisitorFace(m.FaceEmbedding, m.ProfileID, m.VisitorName, safe, h.FaceClient)
+	if err != nil {
+		log.Printf("[new_visitor_face] error: %v", err)
+	}
+}
+
+// process the audio sent from use mic by transcribing it to text
+func (h *WebSocketHandler) handleAudio(m Message, safe *service.SafeConn) {
+	defer service.Wg.Done()
+	err := service.ProcessAudio(m.AudioData, m.ProfileID, h.AudioClient)
+	if err != nil {
+		log.Printf("[audio] error: %v", err)
+	}
+}
+
+// Saves the transcript to the db and calls a gRPC service to generate briefing
+func (h *WebSocketHandler) handleSaveAudioTranscript(m Message) {
+	ctx := context.Background()
+	service.FlushAudio(ctx, m.ProfileID, h.AudioClient)
+	err := service.SaveTranscriptWithRetry(ctx, m.ProfileID, m.VisitorIDList, h.AudioClient)
+	if err != nil {
+		log.Printf("error saving transcript: %v", err)
+	} else {
+		visitorIDs := service.GetSeenVisitors()
+		go func(m Message) {
+			err = service.GenerateConversationBriefing(m.ProfileID, visitorIDs, h.BriefingClient)		
+			if err != nil {
+				log.Printf("[convo briefing] error: %v", err)
+			}
+		}(m)
+	}
+	service.ClearSeenVisitors()
 }
