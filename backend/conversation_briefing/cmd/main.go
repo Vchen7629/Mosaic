952644f26b6b464b5a6b6d1718d85e4c6cb42ab6
Vchen7629 <chenvincent7629@gmail.com>
2026-03-19 18:19:06 -0700

    refactor(convo_briefing): initialized structured logger with slog.Logger

--- backend/conversation_briefing/cmd/main.go
+++ backend/conversation_briefing/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"log/slog"
 	"net"
 	"os"
 	"os/signal"
@@ -21,26 +22,27 @@ type Config struct {
 	ServerPort  string `envconfig:"SERVER_PORT" default:"30030"`
 	DatabaseURL string `envconfig:"DATABASE_URL" default:""`
 	LLMBaseURL  string `envconfig:"OLLAMA_BASE_URL" default:""`
+	ProdMode 	bool   `envconfig:"PROD_MODE" default:"false"`
 }
 
-func gRPCServer(cfg *Config, pool *pgxpool.Pool) (*grpc.Server, error) {
+func gRPCServer(logger *slog.Logger, cfg *Config, pool *pgxpool.Pool) (*grpc.Server, error) {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", cfg.ServerPort))
 	if err != nil {
 		return nil, fmt.Errorf("failed to listen: %w", err)
 	}
 
-	dbPool := db.NewDBPool(pool)
+	dbPool := db.NewDBPool(pool, logger)
 
 	gRPCServer := grpc.NewServer()
 	cb.RegisterConversationBriefingServiceServer(
-		gRPCServer, handler.NewConvoBriefingServer(dbPool, cfg.LLMBaseURL),
+		gRPCServer, handler.NewConvoBriefingServer(logger, dbPool, cfg.LLMBaseURL),
 	)
 
 	go func() {
-		log.Printf("conversation briefing gRPC server listening on: %s", cfg.ServerPort)
+		logger.Info("conversation briefing gRPC server listening on", "port", cfg.ServerPort)
 		err = gRPCServer.Serve(lis)
 		if err != nil {
-			log.Fatalf("failed to serve gRPC server: %v", err)
+			logger.Error("failed to serve gRPC server", "err", err,)
 		}
 	}()
 
@@ -53,14 +55,24 @@ func main() {
 		log.Fatalf("failed to load config values: %v", err)
 	}
 
-	log.Println("Starting Service...")
-	pool := db.ConnectionPool(cfg.DatabaseURL)
+	var handler slog.Handler
+	if cfg.ProdMode {
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})
+	} else {
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})		
+	}
+	logger := slog.New(handler).With("service", "conversation_briefing")
+
+	logger.Info("Starting gRPC server...")
+
+	pool := db.ConnectionPool(logger, cfg.DatabaseURL)
 
 	defer pool.Close()
 
-	server, err := gRPCServer(cfg, pool)
+	server, err := gRPCServer(logger, cfg, pool)
 	if err != nil {
-		log.Fatalf("failed to start gRPC server: %v", err)
+		logger.Error("failed to start gRPC server", "err", err)
+		os.Exit(1)
 	}
 
 	sigChan := make(chan os.Signal, 1)
@@ -68,12 +80,12 @@ func main() {
 
 	// Block until shutdown signal recieved
 	<-sigChan
-	log.Println("Shutting down gracefully...")
+	logger.Info("Shutting down gracefully...")
 
 	server.GracefulStop()
 	pool.Close()
 
-	log.Println("Closed gRPC and dbpool connection")
+	logger.Info("Closed gRPC and dbpool connection")
 }
 
 // method to load config values
