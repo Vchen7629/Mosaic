06e3cbfdcf842451dbe3299d99392b785eb32d89
Vchen7629 <chenvincent7629@gmail.com>
2026-03-19 20:13:50 -0700

    refactor(audio_transcription): updated server.py new structured logs

--- backend/audio_transcription/src/server.py
+++ backend/audio_transcription/src/server.py
@@ -1,13 +1,16 @@
 from .db.connection_pool import create_connection_pool
 from concurrent.futures import ThreadPoolExecutor
 from .grpc.servicer import AudioTranscriptionServicer
+from .core.logging import logger
 from .core.settings import settings
 from .gen import audio_transcription_pb2_grpc
 import grpc
 import signal
 
+
 def handle_shutdown(server, _sig, _frame):
     server.stop(grace=5)
+
 
 def serve():
     server = grpc.server(ThreadPoolExecutor(max_workers=settings.max_workers))
@@ -16,17 +19,21 @@ def serve():
     audio_transcription_pb2_grpc.add_AudioTranscriptionServiceServicer_to_server(
         AudioTranscriptionServicer(db_pool), server
     )
-    server.add_insecure_port(f'[::]:{settings.grpc_server_port}')
-    print(f"Server running on [::]:{settings.grpc_server_port}")
+    server.add_insecure_port(f"[::]:{settings.grpc_server_port}")
     server.start()
+    logger.info("gRPC server running on", port=f"[::]:{settings.grpc_server_port}")
 
     signal.signal(signal.SIGINT, lambda sig, frame: handle_shutdown(server, sig, frame))
-    signal.signal(signal.SIGTERM, lambda sig, frame: handle_shutdown(server, sig, frame))
+    signal.signal(
+        signal.SIGTERM, lambda sig, frame: handle_shutdown(server, sig, frame)
+    )
 
     try:
         server.wait_for_termination()
     finally:
         db_pool.close()
+        logger.info("server stopped")
+
 
 if __name__ == "__main__":
     serve()
